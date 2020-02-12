8134b792c83908aaefa71f89cf596b235425a837
Boggs <Evan.Boggs@mantech.com>
2020-02-12 08:13:27 -0500

    tableau public is neat

--- src/VizzFunc.js
+++ src/VizzFunc.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import axios from "axios";
 import "tableau-api";
 
 function Test2() {
@@ -6,12 +7,15 @@ function Test2() {
     initViz2();
   }, []);
 
+  var viz, workbook, worksheet;
+
   const initViz2 = () => {
-    const vizUrl =
-      "http://public.tableau.com/views/RegionalSampleWorkbook/Storms";
+    const vizUrl = `https://public.tableau.com/views/WojBombs/WojBombs?:display_count=n&:origin=viz_share_link`;
+    // const vizUrl = `https://public.tableau.com/views/MigrantdeathsanddisappearancesintheMediterranean/MigrantsintheMediterranean?:display_count=n&:origin=viz_share_link`;
+    // const vizUrl = `http://public.tableau.com/views/RegionalSampleWorkbook/Storms`;
     const vizContainer = document.getElementById("vizContainer");
-    // let viz = new window.tableau.Viz(vizContainer, vizUrl);
-    new window.tableau.Viz(vizContainer, vizUrl);
+    viz = new window.tableau.Viz(vizContainer, vizUrl);
+    // new window.tableau.Viz(vizContainer, vizUrl);
   };
 
   return (
